15bed5849e2d3e14c594cb936804323ee8ff7618
TSGoose <mrprokst@gmail.com>
2024-07-06 19:44:32 +0400

    Layout of the voting content

---
+++ resources/views/livewire/voting-content.blade.php
@@ -0,0 +1,9 @@
+<div class="mx-60">
+    <livewire:cards.member-horizontal/>
+    <livewire:cards.member-horizontal/>
+    <livewire:cards.member-horizontal/>
+    <livewire:cards.member-horizontal/>
+    <livewire:cards.member-horizontal/>
+    <livewire:cards.member-horizontal/>
+    <livewire:cards.member-horizontal/>
+</div>
