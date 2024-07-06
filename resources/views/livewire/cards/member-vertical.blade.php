f70a993d523f1f6a36ffc9fc74ad86d5b8058d38
TSGoose <mrprokst@gmail.com>
2024-07-06 14:57:35 +0400

    Create model and first component "card vertical"

---
+++ resources/views/livewire/cards/member-vertical.blade.php
@@ -0,0 +1,33 @@
+<article class="flex flex-col rounded-lg shadow-sm max-w-[370px]">
+
+  <img loading="lazy" src="https://cdn.builder.io/api/v1/image/assets/TEMP/adef5a7fcac817fadda5d3f81a3f5e69b6030a96fa301d80c3787b8852850cd6?apiKey=91f2b3c7e1024281a6c7da0dfb0eaca6&" class="w-full aspect-[1.85]" alt="Утро в сосновом лесу painting" />
+
+  <div class="flex flex-col px-9 py-8 w-full bg-white">
+
+    <h2 class="text-2xl font-semibold leading-7 text-center text-gray-900">
+
+      Утро в сосновом лесу
+
+    </h2>
+
+    <p class="mt-4 text-base leading-6 text-center text-gray-500">
+
+      «Утро в сосновом лесу» — картина русских живописцев Ивана Шишкина и Константина Савицкого, написанная в 1889 году
+
+    </p>
+
+    <div class="flex gap-5 justify-between py-1 mt-7">
+
+      <img loading="lazy" src="https://cdn.builder.io/api/v1/image/assets/TEMP/7221698c2ae5e82a52bda1ba0e9966f9eff68394a12a265ce7fdfa748d5b4b9f?apiKey=91f2b3c7e1024281a6c7da0dfb0eaca6&" class="shrink-0 w-8 aspect-square fill-zinc-700" alt="Audio player icon" />
+
+      <div class="flex flex-col justify-center my-auto rounded-full bg-slate-100">
+
+        <div class="shrink-0 h-2 rounded-full bg-zinc-700" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+
+      </div>
+
+    </div>
+
+  </div>
+
+</article>
