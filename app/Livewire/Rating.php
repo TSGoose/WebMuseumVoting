bc5082022282157c62fda5657497ce98ff8995a7
TSGoose <mrprokst@gmail.com>
2024-07-08 01:18:27 +0400

    Rating voting

--- app/Livewire/Rating.php
+++ app/Livewire/Rating.php
@@ -2,12 +2,22 @@
 
 namespace App\Livewire;
 
+use App\Models\VotingMember;
 use Livewire\Component;
 
 class Rating extends Component
 {
+    public $index = 1;
+
     public function render()
     {
-        return view('livewire.rating');
+        $sumVotes = VotingMember::sum('votes');
+
+        if ($sumVotes == 0) $sumVotes ++;
+        return view('livewire.rating', ['members' => 
+        VotingMember::select()           
+        ->orderBy('votes', 'desc')   
+        ->take(5)                          
+        ->get(), 'sumVotes' => $sumVotes]);
     }
 }
--- resources/views/livewire/rating.blade.php
+++ resources/views/livewire/rating.blade.php
@@ -10,36 +10,15 @@
       </tr>
     </thead>
     <tbody>
-      <tr class="grid grid-cols-4 gap-5 mt-6 max-md:mt-10">
-        <td class="text-center text-gray-800 flex items-center justify-center">1</td>
-        <td class="text-center text-gray-900 flex items-center justify-center">Утро в сосновом лесу</td>
-        <td class="text-center text-emerald-500 flex items-center justify-center">536</td>
-        <td class="text-center text-sky-500 flex items-center justify-center">48%</td>
-      </tr>
-      <tr class="grid grid-cols-4 gap-5 mt-12 max-md:mt-10">
-        <td class="text-center text-gray-800 flex items-center justify-center">2</td>
-        <td class="text-center text-gray-900 flex items-center justify-center">Девятый вал</td>
-        <td class="text-center text-emerald-500 flex items-center justify-center">428</td>
-        <td class="text-center text-sky-500 flex items-center justify-center">38%</td>
-      </tr>
-      <tr class="grid grid-cols-4 gap-5 mt-12 max-md:mt-10">
-        <td class="text-center text-gray-800 flex items-center justify-center">3</td>
-        <td class="text-center text-gray-900 flex items-center justify-center">Грачи прилетели</td>
-        <td class="text-center text-emerald-500 flex items-center justify-center">357</td>
-        <td class="text-center text-sky-500 flex items-center justify-center">32%</td>
-      </tr>
-      <tr class="grid grid-cols-4 gap-5 mt-12 max-md:mt-10">
-        <td class="text-center text-gray-800 flex items-center justify-center">4</td>
-        <td class="text-center text-gray-900 flex items-center justify-center">Богатыри</td>
-        <td class="text-center text-emerald-500 flex items-center justify-center">291</td>
-        <td class="text-center text-sky-500 flex items-center justify-center">26%</td>
-      </tr>
-      <tr class="grid grid-cols-4 gap-5 mt-12 max-md:mt-10">
-        <td class="text-center text-gray-800 flex items-center justify-center">5</td>
-        <td class="text-center text-gray-900 flex items-center justify-center">Иван Грозный и сын его Иван</td>
-        <td class="text-center text-emerald-500 flex items-center justify-center">245</td>
-        <td class="text-center text-sky-500 flex items-center justify-center">22%</td>
-      </tr>
+      @foreach($members as $member)
+        
+        <tr class="grid grid-cols-4 gap-5 mt-6 max-md:mt-10">
+          <td class="text-center text-gray-800 flex items-center justify-center">{{$index ++}}</td>
+          <td class="text-center text-gray-900 flex items-center justify-center">{{$member->title}}</td>
+          <td class="text-center text-emerald-500 flex items-center justify-center">{{$member->votes}}</td>
+          <td class="text-center text-sky-500 flex items-center justify-center">{{$member->votes / $sumVotes * 100}}%</td>
+        </tr>
+      @endforeach
     </tbody>
   </table>
 </section>
