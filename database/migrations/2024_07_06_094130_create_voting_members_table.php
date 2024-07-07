850d3c71bece994153a65e753f65572bf8cb1aea
TSGoose <mrprokst@gmail.com>
2024-07-07 14:20:42 +0400

    Structure db

--- database/migrations/2024_07_06_094130_create_voting_members_table.php
+++ database/migrations/2024_07_06_094130_create_voting_members_table.php
@@ -13,6 +13,12 @@ return new class extends Migration
     {
         Schema::create('voting_members', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->string('author');
+            $table->string('images');
+            $table->string('music')->nullable();;
+            $table->integer('votes')->default(0);
             $table->timestamps();
         });
     }
