974de585d8c0558f37861042e15ca8a448897ea6
PiRoGoK112 <mkd112112@gmail.com>
2024-07-08 02:14:45 +0400

    Vertical arrows and music

--- resources/views/livewire/cards/member-vertical.blade.php
+++ resources/views/livewire/cards/member-vertical.blade.php
@@ -1,12 +1,15 @@
 <article class="flex flex-col rounded-lg shadow-sm max-w-[370px]">
 
-  <div  class="flex justify-between items-center w-full aspect-[1.85] rounded-t-lg bg-cover bg-center bg-[url('https://cdn.builder.io/api/v1/image/assets/TEMP/50c8f2882bca7fabe913eaa22930fec71368321080635b3c15be6ea6c588c493?apiKey=6af3166b190849ae913a27151a2b7816&')]" alt="Утро в сосновом лесу painting">
+  <div  class="flex overflow-hidden relative justify-between items-center w-full aspect-[1.85] rounded-t-lg " alt="">
     <img src="{{ $images[$indexImg] }}" alt="">
-    <button type="button" wire:click="left" class = "flex justify-center items-center h-full w-2/12 bg-gray-900 bg-opacity-0 hover:bg-opacity-40 px-5 rounded-tl-lg"><livewire:buttons.arrow/></button> 
-    <button type="button" wire:click="right" class = "flex justify-center items-center h-full w-2/12 bg-gray-900 bg-opacity-0 hover:bg-opacity-40 px-5 rounded-tl-lg scale-x-[-1]"><livewire:buttons.arrow/></button> 
+    <div class = "absolute z-2 w-full h-full flex justify-row justify-between">
+      <button type="button" wire:click="left" class = "flex justify-center items-center h-full w-2/12 bg-gray-900 bg-opacity-0 hover:bg-opacity-40 px-5 rounded-tl-lg"><livewire:buttons.arrow/></button> 
+      <button type="button" wire:click="right" class = "flex justify-center items-center h-full w-2/12 bg-gray-900 bg-opacity-0 hover:bg-opacity-40 px-5 rounded-tl-lg scale-x-[-1]"><livewire:buttons.arrow/></button> 
+    </div>
   </div>
 
-  <div class="flex flex-col px-9 py-8 w-full bg-white">
+
+  <div class="flex flex-col px-9 py-8 w-full bg-white items-center">
 
     <h2 class="text-2xl font-semibold leading-7 text-center text-gray-900">
 
@@ -26,21 +29,17 @@
 
     </p>
 
-    <div class="flex gap-5 justify-between py-1 mt-7">
+    <p class="text-base font-medium leading-6 mt-4 text-gray-500 max-w-[89px]">
 
-      <img loading="lazy" src="https://cdn.builder.io/api/v1/image/assets/TEMP/7221698c2ae5e82a52bda1ba0e9966f9eff68394a12a265ce7fdfa748d5b4b9f?apiKey=91f2b3c7e1024281a6c7da0dfb0eaca6&" class="shrink-0 w-8 aspect-square fill-zinc-700 " alt="Audio player icon" />
+    Голосов: {{ $member->votes }}
 
-      <div class="flex flex-col justify-center my-auto rounded-full bg-slate-100">
-
-        <div class="shrink-0 h-2 rounded-full bg-zinc-700" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
-
-      </div>
- 
+    </p>
 
     </div>
-    <div class="self-center text-center mt-1">
-              <p class="text-base font-medium leading-6 text-gray-500 max-w-[89px]">Голосов: {{ $member->votes }}</p>
-    </div> 
+    <div class = "flex flex-row justify-between items-center">
+        <audio controls src="music1.mp3" class = "bg-blue-600   saturate-200   rounded-b-lg w-full"></audio>
+    </div>
+
   </div>
 
 </article>
--- resources/views/livewire/example-of-member.blade.php
+++ resources/views/livewire/example-of-member.blade.php
@@ -3,7 +3,7 @@
   <h2 class="self-center text-5xl font-black text-center text-black max-md:text-4xl">
     Примеры работ
   </h2>
-  <div class="flex flex-col items-center mt-16 w-full max-md:mt-10 max-md:max-w-full">
+  <div class="flex flex-row justify-around items-center mt-16 w-full max-md:mt-10 max-md:max-w-full">
     <div class="flex gap-40">
         @foreach($members as $member)
         <!--<livewire:cards.member-horizontal :member = $member/>  @include("livewire.cards.member-horizontal", ['member' => $member])-->
