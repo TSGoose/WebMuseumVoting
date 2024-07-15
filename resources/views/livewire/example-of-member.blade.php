e3e129e3f8472c2392268b75e3927c4f21f6ac6e
PiRoGoK112 <mkd112112@gmail.com>
2024-07-15 10:43:56 +0400

    Mainpage adaptive fixed

--- resources/views/livewire/example-of-member.blade.php
+++ resources/views/livewire/example-of-member.blade.php
@@ -3,17 +3,17 @@
     <h2 class="self-center text-5xl font-black text-center text-black max-md:text-4xl">
       Примеры работ
     </h2>
-    <div>
-      <div class="flex items-center flex-col xl:flex-row justify-center items-center mt-16 w-full max-md:mt-10 max-md:max-w-full">
-        <div class="flex justify-between items-center content-center flex-col xl:flex-row gap-10 w-full">
-          <div>
+    <div class="w-full">
+      <div class="flex items-center flex-col xl:flex-row justify-center mt-16 w-full max-md:mt-10 max-md:max-w-full">
+        <div class="flex justify-between items-center content-center flex-col xl:flex-row gap-5 w-full">
+
             @foreach($members as $member)
               <!-- <livewire:cards.member-horizontal :member = $member/> @#include("livewire.cards.member-horizontal", ['member' => $member]) -->
                 <div>
                   @livewire('cards.member-vertical', ['member' => $member], key('member_vert-'.$member->id))
                 </div>
             @endforeach
-          </div>
+
         </div>
       </div>
     </div>
